de1869f384e2c66d9bfb1d18127a3853b27874a4
Edied Isaías Ramos Hernández <edieisa22@gmail.com>
2021-07-01 14:50:46 -0500

    Add files via upload

---
+++ UVa Online Judge/uHunt/Mathematics/10007.py
@@ -0,0 +1,20 @@
+# coder: Yeshayah
+# This problem is about catalan's numbers and about factorials
+
+cat = []
+fact = []
+
+cat.append(1)
+fact.append(1)
+
+for i in range(1, 302):
+  fact.append(i * fact[i - 1])
+
+for i in range(1, 302):
+  cat.append(((2 * i * (2 * i - 1)) * cat[i - 1]) // ((i + 1) * i))
+
+n = int(input())
+
+while n != 0:
+  print(cat[n] * fact[n])
+  n = int(input())
