01099da36371c04e1dad1837411702d12f896ed0
Edied Isaías Ramos Hernández <edieisa22@gmail.com>
2021-07-02 23:10:36 -0500

    Add files via upload

---
+++ UVa Online Judge/uHunt/Mathematics/136.cpp
@@ -0,0 +1,11 @@
+// coder: Yeshayah
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout << "The 1500'th ugly number is 859963392.\n";
+  return 0;
+}
