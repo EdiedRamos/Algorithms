8fe867298e0c6176e0683628ef67eb9e8f813b16
Edied Isaías Ramos Hernández <edieisa22@gmail.com>
2024-12-26 23:56:23 -0500

    Update ans.js

--- LeetCode/1941/ans.js
+++ LeetCode/1941/ans.js
@@ -2,12 +2,9 @@
  * @param {string} s
  * @return {boolean}
  */
-var areOccurrencesEqual = function(s) {
-    const frecuency = {};
-    for (const char of s) {
-      if (frecuency[char] === undefined) frecuency[char] = 0;
-      frecuency[char]++;
-    }
-    const values = Object.values(frecuency);
-    return Math.min(...values) === Math.max(...values);
+var areOccurrencesEqual = function (s) {
+  const frecuency = {};
+  for (const char of s)
+    frecuency[char] = (frecuency[char] ?? 0) + 1;
+  return new Set(Object.values(frecuency)).size === 1;
 };
