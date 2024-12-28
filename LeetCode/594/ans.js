fd1cd7a960588d8c9bbe62714d4ae6c8ff2743b6
Edied Isaías Ramos Hernández <edieisa22@gmail.com>
2024-12-28 01:47:56 -0500

    Create ans.js

---
+++ LeetCode/594/ans.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLHS = function (nums) {
+  nums.sort((a,b) => a - b);
+  let start = 0, end = 1, ans = 0;
+  while (start < nums.length) {
+    while (end < nums.length && (nums[end] === nums[start] || (nums[end] - 1  === nums[start]))) {
+      end++;
+    }
+    if (nums[end - 1] !== nums[start]) {
+      ans = Math.max(ans, end - start);
+    }
+    start++;
+  }
+  return ans;
+};
