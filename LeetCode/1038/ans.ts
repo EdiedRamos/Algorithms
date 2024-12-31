1c798dc6eb2dd76436bbc0bbf22268a3bcbb9894
Edied Isaías Ramos Hernández <edieisa22@gmail.com>
2024-12-31 11:43:29 -0500

    Create ans.ts

---
+++ LeetCode/1038/ans.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+type NodeType = TreeNode | null;
+
+function dfs(root: NodeType, sum: number): number {
+  if (!root) return sum;
+  const tVal = root.val;
+  const rightSum = dfs(root.right, sum);
+  root.val += rightSum;
+  return dfs(root.left, root.val);
+}
+
+function bstToGst(root: NodeType): NodeType {
+  dfs(root, 0);
+  return root;
+};
