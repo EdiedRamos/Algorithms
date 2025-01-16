e6c8c47e65ab60b1babce20f1bd122aa428d3e7b
Edied Isaías Ramos Hernández <edieisa22@gmail.com>
2025-01-15 23:14:44 -0500

    Delete LeetCode/1357 directory

--- LeetCode/1357/ans.sql
+++
@@ -1,6 +0,0 @@
--- Write your PostgreSQL query statement below
-select product_name, sum(unit) as unit
-from Products, Orders
-where Products.product_id = Orders.product_id and order_date between '2020-02-01' and '2020-02-29'
-group by product_name
-having sum(unit) > 99;
