b9edcd4e061d4c2b2ce0ea0a8f7fc8713e98cab2
Edied Isaías Ramos Hernández <edieisa22@gmail.com>
2025-02-28 08:39:36 -0500

    Create ans.ts

---
+++ Practice/LeetCode/1282/ans.ts
@@ -0,0 +1,22 @@
+// 1282. Group the People Given the Group Size They Belong To
+
+function groupThePeople(groupSizes: number[]): number[][] {
+  const collection: Record<number, number[][]> = {};
+  for (let i = 0; i < groupSizes.length; i++) {
+    const groupSize = groupSizes[i];
+    if (collection[groupSize] === undefined) {
+      collection[groupSize] = [[i]];
+    } else {
+      if (collection[groupSize][0].length === groupSize) {
+        collection[groupSize].unshift([i]);
+      } else {
+        collection[groupSize][0].push(i);
+      }
+    }
+  }
+  const answer: number[][] = [];
+  for (const [_, value] of Object.entries(collection)) {
+    answer.push(...value);
+  }
+  return answer;
+};
