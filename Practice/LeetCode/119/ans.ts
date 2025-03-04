1cd2bd76ab098360cf317fcb27b71c5c0834c962
EdiedRamos <edieisa22@gmail.com>
2025-03-04 18:54:57 -0500

    feat: solve 119 with ts

---
+++ Practice/LeetCode/119/ans.ts
@@ -0,0 +1,18 @@
+// 119. Pascal's Triangle II
+
+function generate(numRows: number): number[][] {
+  let pascalTriangle: number[][] = [[1]];
+  for (let i = 2; i <= numRows; i++) {
+    pascalTriangle.push(Array.from({ length: i }).map(() => 1));
+    for (let j = 1; j < i - 1; j++) {
+      pascalTriangle[i - 1][j] =
+        pascalTriangle[i - 2][j] + pascalTriangle[i - 2][j - 1];
+    }
+  }
+  return pascalTriangle;
+}
+
+function getRow(rowIndex: number): number[] {
+	const triangle = generate(rowIndex + 1);
+	return triangle[rowIndex];
+};
