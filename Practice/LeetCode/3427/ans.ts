df359450693374ba61ad3a4b2f4ab561ef372594
EdiedRamos <edieisa22@gmail.com>
2025-03-26 18:34:18 -0500

    feat: solve 3427 using ts

---
+++ Practice/LeetCode/3427/ans.ts
@@ -0,0 +1,15 @@
+// * 3427. Sum of Variable Length Subarrays
+
+function subarraySum(nums: number[]): number {
+  let ans = 0;
+  const prefixSum = [0];
+  for (let i = 0; i < nums.length; i++) {
+    prefixSum.push(nums[i] + prefixSum[i]);
+  }
+  console.log({ prefixSum });
+  // for (let i = 0; i < nums.length; i++) {
+  //   const start = Math.max(0, i - nums[i]);
+  //   ans += prefixSum[i + 1] - prefixSum[start];
+  // }
+  return ans;
+}
