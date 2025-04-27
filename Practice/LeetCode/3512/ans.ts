75ce3b62590357ff775577f7395fd86938d9518d
EdiedRamos <edieisa22@gmail.com>
2025-04-27 01:30:18 -0500

    chore: add description to problem 3512

--- Practice/LeetCode/3512/ans.ts
+++ Practice/LeetCode/3512/ans.ts
@@ -1,3 +1,5 @@
+// * 3512. Minimum Operations to Make Array Sum Divisible by K
+
 function minOperations(nums: number[], k: number): number {
   return nums.reduce((prev, cur) => prev + cur, 0) % k;
 }
