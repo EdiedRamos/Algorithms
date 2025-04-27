b5f57d4f226ab1072343c638be6f02824000d3b1
EdiedRamos <edieisa22@gmail.com>
2025-04-27 00:13:41 -0500

    feat: solve 1910 using ts

---
+++ Practice/LeetCode/1910/ans.ts
@@ -0,0 +1,8 @@
+// * 1910. Remove All Occurrences of a Substring
+
+function removeOccurrences(s: string, part: string): string {
+  while (s.includes(part)) {
+    s = s.replace(part, "");
+  }
+  return s;
+}
