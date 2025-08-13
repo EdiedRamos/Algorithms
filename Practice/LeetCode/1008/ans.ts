31dc0f43fc94ed527bdf9d8460998cb1b2268fa6
EdiedRamos <edieisa22@gmail.com>
2025-08-13 00:31:31 -0500

    feat: solve 1008 using ts

---
+++ Practice/LeetCode/1008/ans.ts
@@ -0,0 +1,43 @@
+// * 1008. Construct Binary Search Tree from Preorder Traversal
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function buildNode(val: number): TreeNode {
+  return {
+    val,
+    left: null,
+    right: null,
+  }
+}
+
+function insertIntoBST(root: TreeNode | null, val: number): TreeNode | null {
+  if (!root) return buildNode(val)
+  if (val < root.val) {
+    if (root.left) insertIntoBST(root.left, val)
+    else root.left = buildNode(val)
+  } else {
+    if (root.right) insertIntoBST(root.right, val)
+    else root.right = buildNode(val)
+  }
+  return root
+}
+
+function bstFromPreorder(preorder: number[]): TreeNode | null {
+  let root: TreeNode | null = null
+  preorder.forEach((val) => {
+    root = insertIntoBST(root, val)
+  })
+  return root
+}
