21ae3ed4202ea768a79a087c76cbf45ff92a1c3c
EdiedRamos <edieisa22@gmail.com>
2025-09-23 17:30:25 -0500

    feat: solve 127 using ts

---
+++ Practice/LeetCode/127/ans.ts
@@ -0,0 +1,40 @@
+// * 127. Word Ladder
+
+function hasConection(a: string, b: string): boolean {
+  let diff = 0
+  for (let i = 0; i < a.length; i++) if (a[i] !== b[i]) diff++
+  return diff < 2
+}
+
+function ladderLength(
+  beginWord: string,
+  endWord: string,
+  wordList: string[]
+): number {
+  const pendings: string[] = [beginWord]
+  const distance: Record<string, number> = { [beginWord]: 1 }
+  const used: Record<string, boolean> = {}
+  const remaining: Set<string> = new Set(wordList)
+  distance[endWord] = Infinity
+  while (pendings.length) {
+    const current = pendings.shift()
+    if (used[current!]) continue
+    used[current!] = true
+    for (const option of [...remaining]) {
+      if (!hasConection(current!, option)) continue
+      if (option === endWord) {
+        distance[option] = Math.min(distance[option], distance[current!] + 1)
+        continue
+      }
+      if (!used[option]) {
+        remaining.delete(option)
+        pendings.push(option)
+        distance[option] = Math.min(
+          distance[option] ?? Infinity,
+          distance[current!] + 1
+        )
+      }
+    }
+  }
+  return distance[endWord] === Infinity ? 0 : distance[endWord]
+}
