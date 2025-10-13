306f9100f8595a0e119d36eb9b824f010789218b
EdiedRamos <edieisa22@gmail.com>
2025-10-12 22:00:37 -0500

    feat: solve 59 using ts

---
+++ Practice/LeetCode/59/ans.ts
@@ -0,0 +1,29 @@
+// * 59. Spiral Matrix II
+
+function generateMatrix(n: number): number[][] {
+  const matrix: number[][] = Array.from({ length: n }).map(() =>
+    Array(n).fill(-1)
+  )
+  const xMove = [0, 1, 0, -1]
+  const yMove = [1, 0, -1, 0]
+  let currentValue = 1
+
+  const dfs = (x: number, y: number, direction: number) => {
+    if (
+      x < 0 ||
+      y < 0 ||
+      x === matrix.length ||
+      y === matrix[0].length ||
+      ~matrix[x][y]
+    )
+      return
+    matrix[x][y] = currentValue++
+    const order = [direction]
+    for (let i = direction + 1; i < 4; i++) order.push(i)
+    for (let i = 0; i < direction; i++) order.push(i)
+    for (const d of order) dfs(x + xMove[d], y + yMove[d], d)
+  }
+  dfs(0, 0, 0)
+
+  return matrix
+}
