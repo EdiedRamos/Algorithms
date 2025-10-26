aa1e1e778dbe44120353be9856e543fed63ee318
EdiedRamos <edieisa22@gmail.com>
2025-10-25 19:28:42 -0500

    feat: solve 2965 using ts

---
+++ Practice/LeetCode/2965/ans.ts
@@ -0,0 +1,23 @@
+class ArrayWrapper {
+  private values: number[]
+
+  constructor(nums: number[]) {
+    this.values = nums
+  }
+
+  valueOf(): number {
+    return this.values.reduce((a, b) => a + b, 0)
+  }
+
+  toString(): string {
+    return `[${this.values.join(',')}]`
+  }
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
