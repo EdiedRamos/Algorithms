b6f48e8dd39cf61f9509e7a242889f922e40d264
EdiedRamos <edieisa22@gmail.com>
2025-11-16 17:48:35 -0500

    feat: solve 1513 using ts

---
+++ Practice/LeetCode/1513/ans.ts
@@ -0,0 +1,16 @@
+// * 1513. Number of Substrings With Only 1s
+
+function numSub(s: string): number {
+  const MOD = 1e9 + 7
+  let ans = 0,
+    co = 0
+  for (const c of s) {
+    if (c === '1') co++
+    else {
+      ans = (ans + (co * (co + 1)) / 2) % MOD
+      co = 0
+    }
+  }
+  ans = (ans + (co * (co + 1)) / 2) % MOD
+  return ans
+}
