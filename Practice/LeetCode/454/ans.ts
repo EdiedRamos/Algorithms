02671f702168b31ccc7834b08fa23d3541c89b6a
EdiedRamos <edieisa22@gmail.com>
2026-02-12 00:13:21 -0500

    feat(leetcode): solve 454 using ts

---
+++ Practice/LeetCode/454/ans.ts
@@ -0,0 +1,25 @@
+// 454. 4Sum II
+
+function fourSumCount(
+  nums1: number[],
+  nums2: number[],
+  nums3: number[],
+  nums4: number[],
+): number {
+  let triples = 0;
+  const n = nums1.length;
+  const sumFre: Record<number, number> = {};
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      const sum = nums1[i] + nums2[j];
+      sumFre[sum] = (sumFre[sum] ?? 0) + 1;
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      const sum = nums3[i] + nums4[j];
+      triples += sumFre[-sum] ?? 0;
+    }
+  }
+  return triples;
+}
